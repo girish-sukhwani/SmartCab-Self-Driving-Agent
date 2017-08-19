8813eb7ac6913c8cbfee9903ca3d562105869e43
Girish Sukhwani <girishcr7@gmail.com>
2017-08-19 03:02:50 -0400

    feat: Complete "Inform the Driving Agent" and "Implement a Q-Learning Driving Agent" sections
    The following are the details of the commit:
    	- Completed "Inform the Driving Agent" section of the project.
    	- Completed "Implement a Q-Learning Driving Agent" section of
    	the project.
    	- Successfully answered questions 4,5 and 6.
    	- The section "Improve the Q-Learning Driving Agent" is pending.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -39,7 +39,10 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-
+        self.epsilon = self.epsilon - 0.05
+        if testing:
+            self.epsilon = 0
+            self.alpha = 0
         return None
 
     def build_state(self):
@@ -56,13 +59,13 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         
-        # NOTE : you are not allowed to engineer eatures outside of the inputs available.
+        # NOTE : you are not allowed to engineer features outside of the inputs available.
         # Because the aim of this project is to teach Reinforcement Learning, we have placed 
         # constraints in order for you to learn how to adjust epsilon and alpha, and thus learn about the balance between exploration and exploitation.
         # With the hand-engineered features, this learning process gets entirely negated.
         
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        state = (waypoint, inputs['light'], inputs['left'], inputs['right'], inputs['oncoming'])
 
         return state
 
@@ -75,8 +78,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Calculate the maximum Q-value of all actions for a given state
-
-        maxQ = None
+        maxQ = max(self.Q[state].values())
 
         return maxQ 
 
@@ -90,7 +92,8 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-
+        if self.learning and (not self.Q or state not in self.Q):
+            self.Q[state] = {act:0.0 for act in self.valid_actions}
         return
 
 
@@ -111,9 +114,12 @@ class LearningAgent(Agent):
         # Otherwise, choose an action with the highest Q-value for the current state
         # Be sure that when choosing an action with highest Q-value that you randomly select between actions that "tie".
         if self.learning:
-            pass
+            if self.epsilon > random.random():
+                action = random.choice(self.valid_actions)
+            else:
+                action = random.choice([act for act,q in self.Q[state].iteritems() if q == self.get_maxQ(state)])
         else:
-            action = self.valid_actions[random.randint(0, len(self.valid_actions) - 1)]
+            action = random.choice(self.valid_actions)
         return action
 
 
@@ -127,7 +133,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
-
+        if self.learning:
+            self.Q[state][action] = (1 - self.alpha) * self.Q[state][action] + (self.alpha * reward)
         return
 
 
@@ -163,7 +170,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent, learning=True)
     
     ##############
     # Follow the driving agent
